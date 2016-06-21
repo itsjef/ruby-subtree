8257ec1e058982e1eab98fec1a78948f64e631d9
Duc Anh Tran <td.anh0812@gmail.com>
2016-06-21 22:00:10 +0700

    add comment to explain what i did

--- tree.rb
+++ tree.rb
@@ -9,27 +9,30 @@ end
 class Tree
   attr_accessor :root
 
-  def self.subtree?(node1, node2) 
-    if node2.nil?
+  def self.subtree?(root1, root2)
+    if root2.nil? # a nil node is obviously subtree of any tree
       true
-    elsif node1.nil?
+    elsif root1.nil? # a tree with nil root doen not have any subtree
       false
-    elsif identical? node1, node2
+    elsif identical? root1, root2 # 2 trees with identical root is obviously each other's subtree
       true
     else
-      subtree? node1.left, node2 or
-      subtree? node1.right, node2
+      # not an obvious subtree? 
+      # check if 2nd tree is a subtree at the left or right branch of 1st tree
+      subtree? root1.left, root2 or
+      subtree? root1.right, root2
     end
   end
 
   private
 
   def self.identical?(node1, node2)
-    if node1.nil? and node2.nil?
+    if node1.nil? and node2.nil? # 2 nil nodes are obviously identical
       true
-    elsif node1.nil? or node2.nil?
+    elsif node1.nil? or node2.nil? # but if one of them are nil, then they are not
       false
     else
+      # 2 identical nodes should have their data the same as well as successor nodes'
       node1.data == node2.data and
       identical? node1.left, node2.left and
       identical? node1.right, node2.right
